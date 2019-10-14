855884417512d4314185eef118d123725aac2ea9
Elukoye <lukoyedith@gmail.com>
2019-10-13 23:01:38 -0700

    Multiples of 3 and 5
    Project Euler Q1

--- euler.rb
+++ euler.rb
@@ -1,4 +1,6 @@
-Multiples of 3 and 5
-# list = Array.new(9){|i| i += 1}
-#  result = list.select{|i|  i %3 == 0 || i%5 == 0}
-#  result.inject(:+)
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+# The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+list = Array.new(1000){|i| i += 1}
+result = list.select{|i| i % 3 == 0 || i % 5 == 0}.inject(:+)
